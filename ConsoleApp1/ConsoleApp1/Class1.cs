5c281cef0723d7a9b1075c9f36b2aa065ac5492a
MarkHyde <markhyde.x@mail.ru>
2020-03-23 21:10:17 +0300

    commit2

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Boots
+    {
+
+
+
+
+        private string proizvod;
+        public float size;
+        public double cena;
+        public string Proizvod
+        {
+            get
+            {
+                return proizvod;
+            }
+            set
+            {
+                proizvod = value;
+            }
+
+        }
+        public float Size
+        {
+            get
+            {
+                return size;
+            }
+            set
+            {
+                size = value;
+            }
+
+        }
+        public double Cena
+        {
+            get
+            {
+                return cena;
+            }
+            set
+            {
+                cena = value;
+            }
+
+        }
+
+        public void Zap()
+        {
+            Console.WriteLine("Введите название обуви");
+            proizvod = Console.ReadLine();
+            Console.WriteLine("Введите размер обуви");
+            size = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Введите цену обуви");
+            cena = Convert.ToDouble(Console.ReadLine());
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Обувь:{proizvod}\nРазмер:{size}\nЦена:{cena}");
+        }
+    }
+}
